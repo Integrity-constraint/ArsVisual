728781d23d31a530904ba8fbe119522ffab5c150
Integrity-constraint <berdyugin.arsen@yandex.ru>
2024-11-30 16:43:04 +0500

    фикс багованных поворотов

--- Controls/ResizeThumb.cs
+++ Controls/ResizeThumb.cs
@@ -17,7 +17,7 @@ namespace DiagramDesigner.Controls
         private Adorner adorner;
         private Point transformOrigin;
         private ContentControl designerItem;
-        private DesignerCanvas designerCanvas;
+        private Canvas canvas;
 
         public ResizeThumb()
         {
@@ -32,13 +32,13 @@ namespace DiagramDesigner.Controls
 
             if (this.designerItem != null)
             {
-                this.designerCanvas = VisualTreeHelper.GetParent(this.designerItem) as DesignerCanvas;
+                this.canvas = VisualTreeHelper.GetParent(this.designerItem) as Canvas;
 
-                if (this.designerCanvas != null)
+                if (this.canvas != null)
                 {
                     this.transformOrigin = this.designerItem.RenderTransformOrigin;
-
                     this.rotateTransform = this.designerItem.RenderTransform as RotateTransform;
+
                     if (this.rotateTransform != null)
                     {
                         this.angle = this.rotateTransform.Angle * Math.PI / 180.0;
@@ -48,7 +48,7 @@ namespace DiagramDesigner.Controls
                         this.angle = 0.0d;
                     }
 
-                    AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.designerCanvas);
+                    AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.canvas);
                     if (adornerLayer != null)
                     {
                         this.adorner = new SizeAdorner(this.designerItem);
@@ -58,143 +58,57 @@ namespace DiagramDesigner.Controls
             }
         }
 
-        void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
         {
-            DesignerItem designerItem = this.DataContext as DesignerItem;
-            DesignerCanvas designer = VisualTreeHelper.GetParent(designerItem) as DesignerCanvas;
-
-            if (designerItem != null && designer != null && designerItem.IsSelected)
+            if (this.designerItem != null)
             {
-                double minLeft, minTop, minDeltaHorizontal, minDeltaVertical;
-                double dragDeltaVertical, dragDeltaHorizontal, scale;
+                double deltaVertical, deltaHorizontal;
 
-                IEnumerable<DesignerItem> selectedDesignerItems = designer.SelectionService.CurrentSelection.OfType<DesignerItem>();
-
-                CalculateDragLimits(selectedDesignerItems, out minLeft, out minTop,
-                                    out minDeltaHorizontal, out minDeltaVertical);
-
-                foreach (DesignerItem item in selectedDesignerItems)
+                switch (VerticalAlignment)
                 {
-                    if (item != null && item.ParentID == Guid.Empty)
-                    {
-                        switch (base.VerticalAlignment)
-                        {
-                            case VerticalAlignment.Bottom:
-                                dragDeltaVertical = Math.Min(-e.VerticalChange, minDeltaVertical);
-                                scale = (item.ActualHeight - dragDeltaVertical) / item.ActualHeight;
-                                DragBottom(scale, item, designer.SelectionService);
-                                break;
-                            case VerticalAlignment.Top:
-                                double top = Canvas.GetTop(item);
-                                dragDeltaVertical = Math.Min(Math.Max(-minTop, e.VerticalChange), minDeltaVertical);
-                                scale = (item.ActualHeight - dragDeltaVertical) / item.ActualHeight;
-                                DragTop(scale, item, designer.SelectionService);
-                                break;
-                            default:
-                                break;
-                        }
-
-                        switch (base.HorizontalAlignment)
-                        {
-                            case HorizontalAlignment.Left:
-                                double left = Canvas.GetLeft(item);
-                                dragDeltaHorizontal = Math.Min(Math.Max(-minLeft, e.HorizontalChange), minDeltaHorizontal);
-                                scale = (item.ActualWidth - dragDeltaHorizontal) / item.ActualWidth;
-                                DragLeft(scale, item, designer.SelectionService);
-                                break;
-                            case HorizontalAlignment.Right:
-                                dragDeltaHorizontal = Math.Min(-e.HorizontalChange, minDeltaHorizontal);
-                                scale = (item.ActualWidth - dragDeltaHorizontal) / item.ActualWidth;
-                                DragRight(scale, item, designer.SelectionService);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
+                    case System.Windows.VerticalAlignment.Bottom:
+                        deltaVertical = Math.Min(-e.VerticalChange, this.designerItem.ActualHeight - this.designerItem.MinHeight);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle));
+                        this.designerItem.Height -= deltaVertical;
+                        break;
+                    case System.Windows.VerticalAlignment.Top:
+                        deltaVertical = Math.Min(e.VerticalChange, this.designerItem.ActualHeight - this.designerItem.MinHeight);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + deltaVertical * Math.Cos(-this.angle) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + deltaVertical * Math.Sin(-this.angle) - (this.transformOrigin.Y * deltaVertical * Math.Sin(-this.angle)));
+                        this.designerItem.Height -= deltaVertical;
+                        break;
+                    default:
+                        break;
                 }
-                e.Handled = true;
-            }
-        }
-        private void CalculateDragLimits(IEnumerable<DesignerItem> selectedItems, out double minLeft, out double minTop, out double minDeltaHorizontal, out double minDeltaVertical)
-        {
-            minLeft = double.MaxValue;
-            minTop = double.MaxValue;
-            minDeltaHorizontal = double.MaxValue;
-            minDeltaVertical = double.MaxValue;
 
-            // drag limits are set by these parameters: canvas top, canvas left, minHeight, minWidth
-            // calculate min value for each parameter for each item
-            foreach (DesignerItem item in selectedItems)
-            {
-                double left = Canvas.GetLeft(item);
-                double top = Canvas.GetTop(item);
-
-                minLeft = double.IsNaN(left) ? 0 : Math.Min(left, minLeft);
-                minTop = double.IsNaN(top) ? 0 : Math.Min(top, minTop);
-
-                minDeltaVertical = Math.Min(minDeltaVertical, item.ActualHeight - item.MinHeight);
-                minDeltaHorizontal = Math.Min(minDeltaHorizontal, item.ActualWidth - item.MinWidth);
+                switch (HorizontalAlignment)
+                {
+                    case System.Windows.HorizontalAlignment.Left:
+                        deltaHorizontal = Math.Min(e.HorizontalChange, this.designerItem.ActualWidth - this.designerItem.MinWidth);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + deltaHorizontal * Math.Cos(this.angle) + (this.transformOrigin.X * deltaHorizontal * (1 - Math.Cos(this.angle))));
+                        this.designerItem.Width -= deltaHorizontal;
+                        break;
+                    case System.Windows.HorizontalAlignment.Right:
+                        deltaHorizontal = Math.Min(-e.HorizontalChange, this.designerItem.ActualWidth - this.designerItem.MinWidth);
+                        Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+                        Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + (deltaHorizontal * this.transformOrigin.X * (1 - Math.Cos(this.angle))));
+                        this.designerItem.Width -= deltaHorizontal;
+                        break;
+                    default:
+                        break;
+                }
             }
-        }
-        private void DragTop(double scale, DesignerItem item, SelectionService selectionService)
-        {
-            IEnumerable<DesignerItem> groupItems = selectionService.GetGroupMembers(item).Cast<DesignerItem>();
-            double groupBottom = Canvas.GetTop(item) + item.Height;
-            foreach (DesignerItem groupItem in groupItems)
-            {
-                double groupItemTop = Canvas.GetTop(groupItem);
-                double delta = (groupBottom - groupItemTop) * (scale - 1);
-                Canvas.SetTop(groupItem, groupItemTop - delta);
-                groupItem.Height = groupItem.ActualHeight * scale;
-            }
+
+            e.Handled = true;
         }
 
-        private void DragRight(double scale, DesignerItem item, SelectionService selectionService)
-        {
-            IEnumerable<DesignerItem> groupItems = selectionService.GetGroupMembers(item).Cast<DesignerItem>();
-
-            double groupLeft = Canvas.GetLeft(item);
-            foreach (DesignerItem groupItem in groupItems)
-            {
-                double groupItemLeft = Canvas.GetLeft(groupItem);
-                double delta = (groupItemLeft - groupLeft) * (scale - 1);
-
-                Canvas.SetLeft(groupItem, groupItemLeft + delta);
-                groupItem.Width = groupItem.ActualWidth * scale;
-            }
-        }
-
-        private void DragBottom(double scale, DesignerItem item, SelectionService selectionService)
-        {
-            IEnumerable<DesignerItem> groupItems = selectionService.GetGroupMembers(item).Cast<DesignerItem>();
-            double groupTop = Canvas.GetTop(item);
-            foreach (DesignerItem groupItem in groupItems)
-            {
-                double groupItemTop = Canvas.GetTop(groupItem);
-                double delta = (groupItemTop - groupTop) * (scale - 1);
-
-                Canvas.SetTop(groupItem, groupItemTop + delta);
-                groupItem.Height = groupItem.ActualHeight * scale;
-            }
-        }
-        private void DragLeft(double scale, DesignerItem item, SelectionService selectionService)
-        {
-            IEnumerable<DesignerItem> groupItems = selectionService.GetGroupMembers(item).Cast<DesignerItem>();
-
-            double groupLeft = Canvas.GetLeft(item) + item.Width;
-            foreach (DesignerItem groupItem in groupItems)
-            {
-                double groupItemLeft = Canvas.GetLeft(groupItem);
-                double delta = (groupLeft - groupItemLeft) * (scale - 1);
-                Canvas.SetLeft(groupItem, groupItemLeft - delta);
-                groupItem.Width = groupItem.ActualWidth * scale;
-            }
-        }
         private void ResizeThumb_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             if (this.adorner != null)
             {
-                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.designerCanvas);
+                AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.canvas);
                 if (adornerLayer != null)
                 {
                     adornerLayer.Remove(this.adorner);
@@ -204,4 +118,6 @@ namespace DiagramDesigner.Controls
             }
         }
     }
+
+
 }
