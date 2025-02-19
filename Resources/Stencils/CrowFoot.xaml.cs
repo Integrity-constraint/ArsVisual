f574277a4e15090eb343ec61e223e866c6ad0f8e
Integrity-constraint <berdyugin.arsen@yandex.ru>
2025-02-19 15:21:21 +0500

    crowfoot задумки

--- Resources/Stencils/CrowFoot.xaml.cs
+++ Resources/Stencils/CrowFoot.xaml.cs
@@ -3,6 +3,7 @@ using DiagramDesigner;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,33 +12,84 @@ using System.Windows.Input;
 
 namespace ArsVisual.Resources.Stencils
 {
-    public class CrowFoot : DesignerItem
+    public class CrowFoot : DesignerItem, INotifyPropertyChanged
     {
-        public string TableName { get; set; } = "New Table"; // Добавьте это свойство
+        private string tableName = "New Table";
+        public string TableName
+        {
+            get => tableName;
+            set
+            {
+                if (tableName != value)
+                {
+                    tableName = value;
+                    OnPropertyChanged(nameof(TableName));
+                }
+            }
+        }
+
         public ObservableCollection<TableField> Fields { get; } = new ObservableCollection<TableField>();
 
         public CrowFoot()
         {
-            // Инициализация таблицы с одним полем по умолчанию
             Fields.Add(new TableField());
         }
 
-        public ICommand AddFieldCommand => new RelayCommand(_ => Fields.Add(new TableField()));
+        private ICommand addFieldCommand;
+        public ICommand AddFieldCommand => addFieldCommand ??= new RelayCommand(_ => Fields.Add(new TableField()));
 
-        private void OnDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(typeof(TableField)))
-            {
-                Fields.Add((TableField)e.Data.GetData(typeof(TableField)));
-            }
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    public class TableField
+    public class TableField : INotifyPropertyChanged
     {
-        public string Name { get; set; } = "New Field";
-        public string Type { get; set; } = "varchar";
-        public bool IsPrimaryKey { get; set; }
+        private string name = "New Field";
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string type = "varchar";
+        public string Type
+        {
+            get => type;
+            set
+            {
+                if (type != value)
+                {
+                    type = value;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+        }
+
+        private bool isPrimaryKey;
+        public bool IsPrimaryKey
+        {
+            get => isPrimaryKey;
+            set
+            {
+                if (isPrimaryKey != value)
+                {
+                    isPrimaryKey = value;
+                    OnPropertyChanged(nameof(IsPrimaryKey));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
 }
